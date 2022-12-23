dbbf0310b03f00ed217f61351277f3c021638fb6
Quina12 <91168308+Quina12@users.noreply.github.com>
2022-12-23 19:41:13 +0700

    Update Modul 13-2.R

--- Modul 13-2.R
+++ Modul 13-2.R
@@ -4,7 +4,7 @@ wilcox.test(dataku_quina$Ganjil,dataku_quina$Genap,paired=TRUE)
 
 df_quina = read.delim("clipboard")
 #independent 2-group Mann-Whitney U Test
-wilcox.test(df_quina$caffeinet~df_quina$placebo)
+wilcox.test(df_quina$caffeine, df_quina$placebo)
 #where y is numeric and A is A binary factor
 head(df_quina)
 rank(df_quina$placebo)
